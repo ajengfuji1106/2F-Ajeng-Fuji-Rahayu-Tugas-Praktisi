3a8476ba2e88c38bfc50c9e338e4ea777457c3aa
Ajeng <ajengfuji1106@gmail.com>
2022-11-16 08:47:27 +0700

    first commit

---
+++ database/seeders/CreateUsersSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+
+class CreateUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name'=> 'Super Admin',
+                'email'=> 'admin@admin.com',
+                'type'=> 1, //Super Admin
+                'password'=> bcrypt ('adminadmin'),
+                'email_verified_at'=> Carbon::now()->format('Y-m-d H:i:s')
+            ]
+        ];
+        foreach ($users as $key => $user) {
+            User::create($user);
+        }
+    }
+}
